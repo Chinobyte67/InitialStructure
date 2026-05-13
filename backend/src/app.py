4e8a0dacd1b44f27e963b667afeaa19ff5556109
octa67 <octavio.aquilante@philips.edu.ar>
2026-05-13 10:59:44 -0300

    agregue artista y album a app

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,7 +1,8 @@
+from backend.src.routers import album_router, artista_router
 from fastapi import FastAPI
 
 from src.middlewares.error_middleware import app_error_handler
-from src.routers import auth_router, user_router, cancion_router
+from src.routers import auth_router, user_router, cancion_router, album_router, artista_router
 from src.utils.errors import AppError
 
 app = FastAPI(title="Initial Structure API")
@@ -13,8 +14,8 @@ app.include_router(auth_router.router, prefix="/api")
 # TODO: registrar product_router cuando se implemente
 # app.include_router(product_router.router, prefix="/api")
 app.include_router(cancion_router.router, prefix="/api")
-
-
+app.include_router(album_router.router, prefix="/api")
+app.include_router(artista_router.router, prefix="/api")
 
 @app.get("/health")
 def health():
